72c232fdf764d1a6a19717c2894a1aa1efa70c54
adam klein <adamklein.mail@gmail.com>
2013-07-09 13:53:55 +0300

    fixes

--- app/controllers/riddles_controller.rb
+++ app/controllers/riddles_controller.rb
@@ -1,7 +1,3 @@
 class RiddlesController < InheritedResources::Base
   respond_to :json
-  def create
-    params[:riddle] ||= params["riddle"]
-    create!
-  end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-TFO::Application.config.session_store :cookie_store, key: '_TFO_session'
+TFO::Application.config.session_store :cookie_store, :key => '_TFO_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
